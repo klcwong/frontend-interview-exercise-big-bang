23b64a25c9ecb113a84e14a1517ced50be788e30
kelvinwongdev <wonglongchun@gmail.com>
2025-02-19 21:34:39 +0000

    added module 'react-router-dom'

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Header from './Header/Header'
+import Home from "../routes/Home/Home";
 
 function App() {
   return (
     <>
       <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
     </>
   )
 }
