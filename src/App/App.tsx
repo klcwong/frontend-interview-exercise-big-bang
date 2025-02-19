d0ec9fced91c2c2f8413e1206818becd4e5f6f58
kelvinwongdev <wonglongchun@gmail.com>
2025-02-19 21:29:17 +0000

    made header as a component

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,21 +1,10 @@
-import automataLogo from '../assets/automata.png'
 import './App.css'
+import Header from './Header/Header'
 
 function App() {
   return (
     <>
-      <header className='header'>
-        <div className='header-icon'>
-          <a href="https://automata.tech/" target="_blank" rel="noreferrer">
-            <img src={String(automataLogo)} className="logo automata" alt="Automata logo"/>
-          </a>
-        </div>
-        <h1>Frontend Exercise</h1>
-        <h2>Rock, Paper, Scissors, Lizard, Spock</h2>
-      </header>
-      <div className='test'>
-
-      </div>
+      <Header />
     </>
   )
 }
---
+++ src/App/Header/Header.tsx
@@ -0,0 +1,23 @@
+import automataLogo from '../../assets/automata.png'
+import "./Header.css"
+
+function Header() {
+    return (
+      <>
+        <header className='header'>
+          <div className='header-icon'>
+            <a href="https://automata.tech/" target="_blank" rel="noreferrer">
+              <img src={String(automataLogo)} className="logo automata" alt="Automata logo"/>
+            </a>
+          </div>
+          <h1>Frontend Exercise</h1>
+          <h2>Rock, Paper, Scissors, Lizard, Spock</h2>
+        </header>
+        <div className='test'>
+  
+        </div>
+      </>
+    )
+  }
+  
+  export default Header
