23ffc5e7548465195e61967d316215b06c3f6087
kelvinwongdev <wonglongchun@gmail.com>
2025-02-19 20:59:09 +0000

    updated the style of header

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,13 +4,18 @@ import './App.css'
 function App() {
   return (
     <>
-      <div>
-        <a href="https://automata.tech/" target="_blank">
-          <img src={String(automataLogo)} className="logo automata" alt="Automata logo"/>
-        </a>
+      <header className='header'>
+        <div className='header-icon'>
+          <a href="https://automata.tech/" target="_blank">
+            <img src={String(automataLogo)} className="logo automata" alt="Automata logo"/>
+          </a>
+        </div>
+        <h1>Frontend Exercise</h1>
+        <h2>Rock, Paper, Scissors, Lizard, Spock</h2>
+      </header>
+      <div className='test'>
+
       </div>
-      <h1>Frontend Exercise</h1>
-      <h2>Rock, Paper, Scissors, Lizard, Spock</h2>
     </>
   )
 }
