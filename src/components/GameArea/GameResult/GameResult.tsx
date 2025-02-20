353a3132b67bf0476db397250331ace8d9b5058c
kelvinwongdev <wonglongchun@gmail.com>
2025-02-20 00:54:51 +0000

    updated the layout of the game result

--- src/components/GameArea/GameResult/GameResult.tsx
+++ src/components/GameArea/GameResult/GameResult.tsx
@@ -1,17 +1,54 @@
-import { GameState } from "../GameArea"
+import { useEffect, useState } from "react"
+import ActionIcon from "../../common/ActionIcon/ActionIcon"
+import { GameState, Result } from "../GameArea"
 import "./GameResult.css"
 
 interface Props {
     gameState: GameState
 }
 
+
+
 function GameResult(props: Props) {
+    const { gameState } = props;
+
+    const [resultState, setResultState] = useState({
+        literal: "",
+        icon: "",
+    })
+
+    useEffect(() => {
+        let newLiteral = "";
+        let newIcon = "";
+        switch (gameState.result) {
+            case Result.Win:
+                newLiteral = "You Win";
+                newIcon = "😄";
+                break;
+            case Result.Lose:
+                newLiteral = "You Lose";
+                newIcon = "😢";
+                break;
+            case Result.Tie:
+                newLiteral = "Tie";
+                newIcon = "👔";
+                break;
+            default:
+                break;
+        }
+        setResultState((state) => ({
+            ...state,
+            literal: newLiteral,
+            icon: newIcon,
+        }))
+    }, [gameState])
+
     return (
         <div className="game-result">
             {props.gameState.isPlayed && 
-            (<><h1>You: {props.gameState.player1Action}</h1>
-                <h1>Computer: {props.gameState.player2Action}</h1>
-                <h1>Result: {props.gameState.result}</h1>
+            (<><h2>{resultState.literal} {resultState.icon} </h2>
+                <h4>You: <ActionIcon action={gameState.player1Action}/></h4>
+                <h4>Computer: <ActionIcon action={gameState.player2Action}/></h4>
             </>)
             }
         </div>
